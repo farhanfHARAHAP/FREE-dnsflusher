40c6061f12e1912b759fdab8298d14a111a7d0a9
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2022-03-21 08:23:47 +0700

    Update dnsflush-python.py

--- dnsflush-python.py
+++ dnsflush-python.py
@@ -8,8 +8,10 @@ while True:
     hour[2] += 1
     if hour[2] == 60:
         hour[1] += 1
+        hour[1] = 0
     if hour[1] == 60:
         hour[0] += 1
+        hour[0] = 0
     print('\nTime Running yet:')
     print(hour[0],hour[1],hour[2],sep = ' : ')
     sleep(1)
