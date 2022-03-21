dbf0a3c921428b3b352ce7e322a22c7b542ce16b
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2022-03-21 08:18:42 +0700

    Add files via upload

---
+++ dnsflush-python.py
@@ -0,0 +1,15 @@
+from os import system
+from time import sleep
+
+hour = [0,0,0]
+while True:
+    system("ipconfig /flushdns")
+    system('cls')
+    hour[2] += 1
+    if hour[2] == 60:
+        hour[1] += 1
+    if hour[1] == 60:
+        hour[0] += 1
+    print('\nTime Running yet:')
+    print(hour[0],hour[1],hour[2],sep = ' : ')
+    sleep(1)
